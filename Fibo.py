de00b3a909ada1a8c9a6b3ff1cc351ca715956cf
Wesley Sousa <wesleysousa1121@gmail.com>
2024-10-19 13:19:40 -0300

    Final

---
+++ Fibo.py
@@ -0,0 +1,20 @@
+def fibonacci(n: int):
+    if n == 1:
+        return 1
+    if n == 2:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+    
+
+cont = 1
+num = int(input("Numero que deseja encontrar: "))
+while(num > fibonacci(cont)):
+    cont += 1
+
+resultado_proximo = fibonacci(cont)
+
+if(resultado_proximo == num):
+    print(f"O numero {num} pertence à sequência de Fibonacci")
+else:
+    print(f"O numero {num} não pertence à sequência de Fibonacci, o numero mais proximo é {resultado_proximo}")
